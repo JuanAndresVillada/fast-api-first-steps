b5054a31ccc5135881d1412b370f7cf3eca78580
JuanVilladaPersonal <juanandresvl20@gmail.com>
2021-10-31 18:00:53 -0500

    remove_duplicates

--- main.py
+++ main.py
@@ -46,6 +46,10 @@ class Person(BaseModel):
         )
     hair_color: Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = Field(default=None)
+    password: str = Field(
+        ...,
+        min_length=1,
+        )
 
     # class Config:
     #     schema_extra = {
@@ -58,13 +62,35 @@ class Person(BaseModel):
     #         }   
     #     }
 
+class PersonOut(BaseModel):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        # example = "Juanito"
+        ) 
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        # example = "Villadita"
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115
+        # example = 18
+        )
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
+
 @app.get('/')
 def home():
     return {"Hello" : "World!"}
 
 # Request and Response Body 
 
-@app.post('/person/new')
+@app.post('/person/new', response_model=PersonOut)
 def create_person(person: Person = Body(...)):
     return person
 
