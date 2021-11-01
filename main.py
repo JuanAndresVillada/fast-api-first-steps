2f7bfb9e2503b769b72448f13407bc1a4bace126
JuanVilladaPersonal <juanandresvl20@gmail.com>
2021-10-31 21:05:23 -0500

    summary_and_description

--- main.py
+++ main.py
@@ -91,6 +91,18 @@ def home():
     tags=["Persons"]
     )
 def create_person(person: Person = Body(...)):
+    """
+    Create Person
+
+    This path operation creates a person in the app and save the information in the database
+
+    Parameters: 
+    - Request body parameter: 
+        - **person: Person** -> A person model with first name, last name, age, hair color and marital stauts
+
+    Returns a person model with first name, last name, age, hair color and marital status
+    """
+
     return person
 
 # Validaciones: Query Parameters
